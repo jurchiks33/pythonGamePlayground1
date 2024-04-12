e350bce1b060bdea7a924b8dd9a3835dee887b96
jurchiks33 <jurisslisans@gmail.com>
2024-04-12 17:22:59 +0200

    creating cave choosing function

--- dragonRealm.py
+++ dragonRealm.py
@@ -7,3 +7,11 @@ def displayIntro():
            The other dragon is greedy and hungry, and will eat you on sight.''')
     print()
 
+def chooseCave():
+    cave = ''
+    while cave != '1' and cave != '2':
+        print('Which cave will you go into? (1 or 2)')
+        cave = input()
+
+    return cave
+
