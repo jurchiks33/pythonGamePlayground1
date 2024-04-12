c98d518bad24ef61a7d0a393b794ed6c382fbc5a
jurchiks33 <jurisslisans@gmail.com>
2024-04-12 17:21:19 +0200

    dragon realm intro

---
+++ dragonRealm.py
@@ -0,0 +1,9 @@
+import random
+import time
+
+def displayIntro():
+    print('''You are in a land full of dragons. In front of you, you see
+          two caves. In one cave, the dragon is friendly and will share his treasures with you.
+           The other dragon is greedy and hungry, and will eat you on sight.''')
+    print()
+
