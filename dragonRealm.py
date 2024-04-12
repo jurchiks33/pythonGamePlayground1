78d8e09e89b6e5337d096870e43f0b00e9201237
jurchiks33 <jurisslisans@gmail.com>
2024-04-12 17:42:02 +0200

    updated game mechanic so you can go deeper

--- dragonRealm.py
+++ dragonRealm.py
@@ -4,7 +4,7 @@ import time
 def displayIntro():
     print('''You are in a land full of dragons. In front of you, you see
           two caves. In one cave, the dragon is friendly and will share his treasures with you.
-           The other dragon is greedy and hungry, and will eat you on sight.''')
+          The other dragon is greedy and hungry, and will eat you on sight.''')
     print()
 
 def chooseCave():
@@ -12,7 +12,6 @@ def chooseCave():
     while cave != '1' and cave != '2':
         print('Which cave will you go into? (1 or 2)')
         cave = input()
-
     return cave
 
 def checkCave(chosenCave):
@@ -28,14 +27,26 @@ def checkCave(chosenCave):
 
     if chosenCave == str(friendlyCave):
         print('Gives you treasure!')
+        return True
     else:
         print('Gobbles you down in one bite!')
+        return False
+
+def play():
+    correctGuesses = 0  # Counter for correct guesses
+    displayIntro()
+    while True:
+        caveNumber = chooseCave()
+        if checkCave(caveNumber):
+            correctGuesses += 1
+            print("\nYou venture deeper into the dragon land...")
+        else:
+            break
+        
+    print(f'Game over! You guessed {correctGuesses} caves correctly.')
+    print('Do you want to play again? (yes or no)')
+    return input()
     
 playAgain = 'yes'
 while playAgain == 'yes' or playAgain == 'y':
-    displayIntro()
-    caveNumber = chooseCave()
-    checkCave(caveNumber)
-
-    print('Do you want play again? (yes or no)')
-    playAgain = input()
+    playAgain = play()
