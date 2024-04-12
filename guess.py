81d9e0e240134b7b872e6c40b4b98a4eb4f1f718
jurchiks33 <jurisslisans@gmail.com>
2024-04-12 14:25:27 +0200

    last polishes, like spacings and some misspelling

--- guess.py
+++ guess.py
@@ -6,10 +6,11 @@ guessesTaken = 0
 print('Hello! My name is guessAI and what is your name?')
 myName = input()
 
-number = random.radinat(1, 100)
-print('Alrighty,' + myName + ', Want to guess number i am thinkig?, its between 1 and 100.')
+number = random.randint(1, 100)
+print('Alrighty,' + myName + 
+      ', Want to guess number i am thinkig?, its between 1 and 100. And yeah you have 10 attempts')
 
-for guessesTaken in range(6):
+for guessesTaken in range(10):
     print('Take a guess.')
     guess = input()
     guess = int(guess)
@@ -25,8 +26,7 @@ for guessesTaken in range(6):
 
 if guess == number:
     guessesTaken = str(guessesTaken + 1)
-    print('Guessantastic job,' + myName + 'You guessed my number in' + 
-          guessesTaken + 'guesses!')
+    print('Guessantastic job, ' + myName + '! You guessed my number in ' + guessesTaken + ' guesses!')
 
 if guess !=number:
     number = str(number)
