9e2dc77437e5730fbf7d0344568911f51ffa359f
jurchiks33 <jurisslisans@gmail.com>
2024-04-13 15:00:35 +0200

    Working on tetris

---
+++ Tetris.py
@@ -0,0 +1,13 @@
+import random, time, pygame, sys
+from pygame.locals import *
+
+FPS = 25
+WINDOWWIDTH = 640
+WINDOWHEIGHT = 480
+BOXSIZE = 20
+BOARDWIDTH = 10
+BOARDHEIGHT = 20
+BLANK = ','
+
+MOVESIDEWAYSFREQ = 0.15
+MOVEDOWNFREQ = 0.1
